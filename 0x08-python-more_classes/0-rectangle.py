6cbf09357b9ae3e056328c80e9281b30fd92754a
LucyIbe <Ibelucyifeosoluchukwu@gmail.com>
2023-01-30 20:47:38 +0000

    rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module describes a Rectangle class and it's functionality
+"""
+
+
+class Rectangle:
+        """
+        Rectangle class Implementation
+        """
+        pass
+
